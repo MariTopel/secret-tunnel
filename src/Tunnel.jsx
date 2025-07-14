f58d6accf9808043846daee7a9694692a907fe2e
Mari Topel <maritopel@gmail.com>
2025-07-14 10:52:55 -0500

    added secret tunnel lyrics because it's important

--- src/Tunnel.jsx
+++ src/Tunnel.jsx
@@ -7,7 +7,13 @@ export default function Tunnel() {
         The earth beneath your feet trembles as the badgers slowly push the two
         doors of the gate open.
       </p>
-      <p>You step into the tunnel, and your journey continues.</p>
+      <p>
+        You step into the tunnel, and your journey continues. Two lovers,
+        forbidden from one another A war divides their people And a mountain
+        divides them apart Built a path to be together ...Yeah, and I forget the
+        next couple of lines, but then it goes... Secret tunnel! Secret tunnel!
+        Through the mountain! Secret, secret, secret, secret tunnel!"
+      </p>
     </section>
   );
 }
