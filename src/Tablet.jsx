95b9ad0a9bdcfd86d525e15e4facc9f1e1cc4b24
Mari Topel <maritopel@gmail.com>
2025-07-14 13:50:38 -0500

    finished tablet and tunnel. App.jsx seems to work just fine with it all on test

--- src/Tablet.jsx
+++ src/Tablet.jsx
@@ -1,6 +1,19 @@
 /** Button that attempts to use the token in context when clicked */
+import { useAuth } from "./AuthContext";
+
 export default function Tablet() {
-  // TODO: call authenticate when form is submitted
+  //calls useAuth hook to get context value
+  //remember useAuth returns an object. this commands javascript to find the property inside the object named authenticate and create a new const called authenticate with that value
+  const { authenticate } = useAuth();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await authenticate(); // call the context’s authenticate()
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <section>
@@ -16,8 +29,8 @@ export default function Tablet() {
       <p>
         It holds out a rectangular stone tablet carved with an intricate design.
       </p>
-      <form>
-        <button>Place your palm upon the tablet.</button>
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Place your palm upon the tablet.</button>
       </form>
     </section>
   );
